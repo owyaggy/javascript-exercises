cb6d2ca33439b370d2a8466bb2ffb606d4b21fd5
Owen Yaggy <owyaggy@icloud.com>
2025-12-04 00:51:33 -0500

    Add JS to solve 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (int1 < 0 || int2 < 0 || Math.floor(int1) !== int1 || Math.floor(int2) !== int2) return 'ERROR';
+    let sum = 0;
+    for (let i = Math.min(int1, int2); i <= Math.max(int1, int2); i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
