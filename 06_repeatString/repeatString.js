839680dfbcbf93c475489668dfff0968def16503
Owen Yaggy <owyaggy@icloud.com>
2025-12-04 00:26:48 -0500

    Add JS to solve 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR';
+    returnString = '';
+    while (num > 0) {
+        returnString += string;
+        num--;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
