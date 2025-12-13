8f63b8ddb1ae7347b58abf2dbc522d701c0a1cfc
Owen Yaggy <owyaggy@icloud.com>
2025-12-13 14:55:16 -0500

    Add solution for 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    
+    let strArr = str.split('');
+    strArr = strArr.filter(char => 'abcdefghijklmnopqrstuvexyz1234567890'.includes(char));
+    let start = 0;
+    let end = strArr.length - 1; 
+    while (start < end) {
+        if (strArr[start] !== strArr[end]) return false;
+        start += 1;
+        end -= 1;
+    }
+    return true;
 };
 
 // Do not edit below this line
