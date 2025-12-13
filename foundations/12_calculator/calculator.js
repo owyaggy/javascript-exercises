31bc10877a719288f8055c0f840eee22061750da
Owen Yaggy <owyaggy@icloud.com>
2025-12-13 14:32:25 -0500

    Add solutions for 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+  return args.reduce((total, current) => total += current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return args.reduce((total, current) => total *= current, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
