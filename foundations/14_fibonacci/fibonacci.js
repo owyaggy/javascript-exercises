ba7a9d50a39fd08c2be183512513e0cb105d2006
Owen Yaggy <owyaggy@icloud.com>
2025-12-13 15:05:03 -0500

    Add solution for 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+   if (n < 0) return "OOPS";
+   let sum = 0;
+   let a = 0;
+   let b = 1;
+   for (let i = 0; i < n; i++) {
+    sum = a + b;
+    b = a;
+    a = sum;
+   }
+   return sum;
 };
 
 // Do not edit below this line
